f9052e4d4a72a3fca22ae36c5f955047976fd964
Shevek <gtophuria@gmail.com>
2021-01-24 20:33:52 +0400

    :lipstick: Updated results page's styles

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,10 +1,14 @@
 import React from "react";
+import Loser from "../assets/youknownothing.gif";
+import Winner from "../assets/iknowsomething.gif";
 
 const Results = ({ score, handleRestart }) => {
   return (
     <>
       <div className="results">
-        <h2>Your Score: {score}/10</h2>
+        <h1>Your Score: {score}/10</h1>
+        {score <= 5 && <img src={Loser} alt="Loser" />}
+        {score > 5 && <img src={Winner} alt="Winner" />}
       </div>
       <div className="buttons">
         <button className="restart" onClick={handleRestart}>
